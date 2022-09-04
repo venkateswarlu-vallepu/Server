6361957acb4b08f200a8c084c258435fb93211db
venkateswarlu-vallepu <108612511+venkateswarlu-vallepu@users.noreply.github.com>
2022-09-04 15:26:05 +0530

    Changes done as per the requirment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useRef } from "react";
+
 
 function App() {
+  let display = useRef();
+  let displaymonths = useRef();
+  let displaydays = useRef();
+ 
+    
+   
+
+
+   let getDataFromServer = async () =>{
+    let reqOtions = {
+      method:"GET"
+    };
+
+    let rawData = await fetch("http://localhost:6969/planets", reqOtions);
+
+    let convertedData = await rawData.json();
+    display.current.innerHTML = convertedData;
+   }
+   let getDataFromServer1 = async () =>{
+    let reqOtions = {
+      method:"GET"
+    };
+
+    let rawData = await fetch("http://localhost:6969/months", reqOtions);
+
+    let convertedData = await rawData.json();
+    displaymonths.current.innerHTML = convertedData;
+  };
+  let getDataFromServer2 = async () =>{
+    let reqOtions = {
+      method:"GET"
+    };
+
+    let rawData = await fetch("http://localhost:6969/weekDays", reqOtions);
+
+    let convertedData = await rawData.json();
+    displaydays.current.innerHTML = convertedData;
+
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='head'>Getting Data from Server</h1>
+      <div>
+     <button className='planetsbutton'  onClick={()=>{
+         getDataFromServer();
+     }}>Req-PlanetsData</button>
+      <h1 className='planets' ref={display}></h1>
+     </div>
+     <div>
+      <button className='planetsbutton' onClick={()=>{
+         getDataFromServer1();
+     }}>Req-MonthsData</button>
+     <h1 className='months' ref={displaymonths}></h1>
+     </div>
+     <div>
+      <button className='planetsbutton' onClick={()=>{
+         getDataFromServer2();
+     }}>Req-WeekDaysData</button>
+     <h1 className='weekdays' ref={displaydays}></h1>
+     </div>
     </div>
   );
 }
